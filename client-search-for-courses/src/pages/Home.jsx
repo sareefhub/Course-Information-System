083f5cf8906989e0b5f5b88a9f027b97a43fee15
Lutfee Dorloh <lutfee2017@gmail.com>
2024-04-12 21:09:23 +0700

    Update

---
+++ client-search-for-courses/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+import "./Home.css"
+
+const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+  
+    const handleChange = (event) => {
+      setSearchTerm(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Filter the data based on the search term
+      console.log(`Searching for: ${searchTerm}`);
+    };
+  
+    return (
+      <div className="home-search-bar">
+        <div className='home-title'>ค้นหารายวิชา</div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="ชื่อวิชาหรือรหัสวิชา"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+          <button type="submit">ค้นหา</button>
+        </form>
+      </div>
+    );
+};
+
+export default Home;
