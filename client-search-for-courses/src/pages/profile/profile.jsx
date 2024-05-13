c0d558fc4f3403875f055be54e3bf90fa3ab7255
iffahan <jackcopsluncher@gmail.com>
2024-05-13 07:20:06 +0700

    profile card

--- client-search-for-courses/src/pages/profile/profile.jsx
+++ client-search-for-courses/src/pages/profile/profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useStudentData } from '../../components/hooks/useStudentData';
 import Navbar from '../../components/navbar/Navbar';
-import { Container, Typography, Card, CardContent, Grid } from '@mui/material';
+import { Container, Typography, Card, CardContent, Grid, Button, Avatar, Box } from '@mui/material';
 import './Profile.css';
 
 function Profile() {
@@ -11,82 +11,66 @@ function Profile() {
     <div>
       <Navbar />
       <Container maxWidth="md">
-                <Grid container spacing={2}>
-                    {studentData && (
-                        <Grid item xs={12}>
-                            <Card sx={{ marginBottom: 2, marginTop: 2, backgroundColor: '#e6f2ff', color: 'black' }} className='title-student'>
-                                <CardContent sx={{ textAlign: 'left' }}>
-                                    <Typography variant="h4" sx={{ marginBottom: 0.5, fontWeight: 'bold' }}>
-                                     ประวัตินักศึกษา
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                            <Grid container spacing={2}>
-                                <Grid item xs={3}>
-                                    <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-                                      รหัสนักศึกษา
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <Typography variant="body1">
-                                      {studentData.studentId}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-                                        ชื่อ
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <Typography variant="body1">
-                                      {studentData.titleNameThai} {studentData.studNameThai} {studentData.studSnameThai}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-                                        ชั้นปีที่
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <Typography variant="body1">
-                                      {studentData.yearStatus}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-                                        กำลังศึกษา
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <Typography variant="body1">
-                                      {studentData.majorNameThai} {studentData.facNameThai}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-                                        ระดับ
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <Typography variant="body1">
-                                      {studentData.studyLevelName} {studentData.studyTypeName}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={3}>
-                                    <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
-                                        วิทยาเขต
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <Typography variant="body1">
-                                     {studentData.campusNameThai}
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    )}
-                </Grid>
-            </Container>
+        <Card sx={{ marginBottom: 2, marginTop: 2 }}>
+          <CardContent>
+            <Typography variant="h5" sx={{ fontWeight: 'bold', mb: 2 }}>
+              ประวัตินักศึกษา
+            </Typography>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={3}>
+                <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)' }}>
+                  รหัสนักศึกษา
+                </Typography>
+                <Typography variant="body1">
+                  {studentData && studentData.studentId}
+                </Typography>
+                <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)', mt: 2 }}>
+                  ชื่อ
+                </Typography>
+                <Typography variant="body1">
+                  {studentData && `${studentData.titleNameThai} ${studentData.studNameThai} ${studentData.studSnameThai}`}
+                </Typography>
+                <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)', mt: 2 }}>
+                  ชั้นปีที่
+                </Typography>
+                <Typography variant="body1">
+                  {studentData && studentData.yearStatus}
+                </Typography>
+                <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)', mt: 2 }}>
+                  กำลังศึกษา
+                </Typography>
+                <Typography variant="body1">
+                  {studentData && `${studentData.majorNameThai} ${studentData.facNameThai}`}
+                </Typography>
+                <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)', mt: 2 }}>
+                  ระดับ
+                </Typography>
+                <Typography variant="body1">
+                  {studentData && `${studentData.studyLevelName} ${studentData.studyTypeName}`}
+                </Typography>
+                <Typography variant="body1" sx={{ color: 'rgba(0, 0, 0, 0.6)', mt: 2 }}>
+                  วิทยาเขต
+                </Typography>
+                <Typography variant="body1">
+                  {studentData && studentData.campusNameThai}
+                </Typography>
+              </Grid>
+              <Grid item xs={9} sx={{ position: 'relative' }}>
+                <Box sx={{ width: 150, height: 150, overflow: 'hidden', position: 'absolute', top: 0, right: 0, borderRadius: '50%', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)' }}>
+                  <Avatar
+                    alt="Student"
+                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&q=80"
+                    sx={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+            <Button variant="outlined" sx={{ mt: 2 }}>
+              แก้ไข
+            </Button>
+          </CardContent>
+        </Card>
+      </Container>
     </div>
   );
 }
